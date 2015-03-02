b17bc57645e43bd4c9fc1e91b8d2f7ce3da045f8
Sambuddha Kishore De <sambuddha.de@gmail.com>
2015-03-02 14:18:17 +0530

    Revert "commit 2"

--- app/models/location.rb
+++
@@ -1,4 +0,0 @@
-class Location < ActiveRecord::Base
-  validates :name, :presence => true
-  #validates :position, :presence => true, numericality: { only_integer: true, message: "Not a valid position." }
-end
